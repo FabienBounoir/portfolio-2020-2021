40a4c42332bd51d67a89aef8cda83f8f35dc0338
fabienbounoir <fabien.bounoir@epitech.eu>
2021-06-11 22:27:46 +0200

    mise a jour du portfolio

--- assets/js/bs-init.js
+++ assets/js/bs-init.js
@@ -1,6 +1,11 @@
+if (window.innerWidth < 768) {
+	$('[data-bss-disabled-mobile]').removeClass('animated').removeAttr('data-aos data-bss-hover-animate');
+}
+
 $(document).ready(function(){
-	AOS.init({ disable: 'mobile' });
-	$('[data-bs-hover-animate]')
-		.mouseenter( function(){ var elem = $(this); elem.addClass('animated ' + elem.attr('data-bs-hover-animate')) })
-		.mouseleave( function(){ var elem = $(this); elem.removeClass('animated ' + elem.attr('data-bs-hover-animate')) });
+	AOS.init();
+
+	$('[data-bss-hover-animate]')
+		.mouseenter( function(){ var elem = $(this); elem.addClass('animated ' + elem.attr('data-bss-hover-animate')) })
+		.mouseleave( function(){ var elem = $(this); elem.removeClass('animated ' + elem.attr('data-bss-hover-animate')) });
 });
